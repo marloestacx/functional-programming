c8b3525d39dacf17f57c1ad535a977b8be319a04
marloes <marloes@musjroom.com>
2021-10-30 13:36:26 +0200

    Hoofdletters verwijderen

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
+//JSON bestand inladen
 fetch("./dataset_raw.json")
-  .then(function (response) {
-    // The JSON data will arrive here
+  .then(function (response) 
+  {
+    // JsSON data
     return response.json();
   })
-  .then(function (data) {
-    appendData(data);
+  .then(function (data) 
+  {
+    parseData(data);
+    removeCapitals(data);
   })
-  .catch(function (err) {
-    // If an error occured, it will be catched here
+  .catch(function (err) 
+  {
+    // Catch error
     console.log(err);
   });
 
-  function appendData(data) {
-    var mainContainer = document.getElementById("dataset");
-    for (var i = 0; i < data.length; i++) {
-        // Loops all items of json file to the console 
-        console.log(data);
+//Data verwerken
+function parseData(data) {
+	/* Deze functie genereert een nieuwe Promise, dit zou ook gebeuren als je de dataset met een fetch() zou ophalen */
+	return new Promise((resolve, reject) => {
+		let dataSet = data;
+		/* resolve stuurt de data naar de .then() onderin */
+		resolve(dataSet);
+	})
+}
+
+//Hoofdletters verwijderen
+  function removeCapitals(data) {
+    //Data omzetten naar string
+    data = JSON.stringify(data)
+    if(typeof data === 'string') 
+    {
+      data = data.toLowerCase();
+      console.log(data.split("{"));
+      return data;
+    } 
+    else 
+    {
+      return data;
     }
   }
